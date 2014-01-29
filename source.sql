41f1e590070112d335290008913b9315eb14e414
ron <ron.kirschler@gmail.com>
2014-01-29 11:37:02 +0100

    Minor changes to source.sql

--- source.sql
+++ source.sql
@@ -5,28 +5,28 @@
 
 
 CREATE TABLE IF NOT EXISTS eav__attributes (
-  id CHAR(36) CHARACTER SET latin1 COLLATE latin1_bin NOT NULL DEFAULT '',
-  entity_id CHAR(36) CHARACTER SET latin1 COLLATE latin1_bin NOT NULL DEFAULT '',
-  attr_name VARCHAR(128) CHARACTER SET latin1 COLLATE latin1_bin NOT NULL DEFAULT '',
-  attr_type ENUM('int','dec','str','date') CHARACTER SET latin1 COLLATE latin1_bin NOT NULL DEFAULT 'str',
-  PRIMARY KEY (id),
-  UNIQUE KEY unique_attribute (entity_id,attr_name),
-  CONSTRAINT FK_eav__attributes_eav__entities FOREIGN KEY (entity_id) REFERENCES eav__entities (id) ON DELETE CASCADE ON UPDATE CASCADE
+	id CHAR(36) CHARACTER SET latin1 COLLATE latin1_bin NOT NULL DEFAULT '',
+	entity_id CHAR(36) CHARACTER SET latin1 COLLATE latin1_bin NOT NULL DEFAULT '',
+	attr_name VARCHAR(128) CHARACTER SET latin1 COLLATE latin1_bin NOT NULL DEFAULT '',
+	attr_type ENUM('int','dec','str','date') CHARACTER SET latin1 COLLATE latin1_bin NOT NULL DEFAULT 'str',
+	PRIMARY KEY (id),
+	UNIQUE KEY unique_attribute (entity_id,attr_name),
+	CONSTRAINT FK_eav__attributes_eav__entities FOREIGN KEY (entity_id) REFERENCES eav__entities (id) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 
 CREATE TABLE IF NOT EXISTS eav__entities (
-  id CHAR(36) CHARACTER SET latin1 COLLATE latin1_bin NOT NULL DEFAULT '',
-  parent_id CHAR(36) CHARACTER SET latin1 COLLATE latin1_bin DEFAULT NULL,
-  entity_create_date datetime DEFAULT '2000-01-01 00:00:00',
-  entity_modify_date timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  entity_path VARCHAR(1024) CHARACTER SET latin1 COLLATE latin1_bin DEFAULT NULL,
-  entity_name VARCHAR(255) CHARACTER SET latin1 COLLATE latin1_bin NOT NULL DEFAULT '',
-  PRIMARY KEY (id),
-  UNIQUE KEY entity_name (entity_name,parent_id),
-  KEY entity_path (entity_path(255)),
-  KEY FK_eav__entities_eav__entities (parent_id),
-  CONSTRAINT FK_eav__entities_eav__entities FOREIGN KEY (parent_id) REFERENCES eav__entities (id) ON DELETE CASCADE ON UPDATE CASCADE
+	id CHAR(36) CHARACTER SET latin1 COLLATE latin1_bin NOT NULL DEFAULT '',
+	parent_id CHAR(36) CHARACTER SET latin1 COLLATE latin1_bin DEFAULT NULL,
+	entity_create_date datetime DEFAULT '2000-01-01 00:00:00',
+	entity_modify_date timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+	entity_path VARCHAR(1024) CHARACTER SET latin1 COLLATE latin1_bin DEFAULT NULL,
+	entity_name VARCHAR(255) CHARACTER SET latin1 COLLATE latin1_bin NOT NULL DEFAULT '',
+	PRIMARY KEY (id),
+	UNIQUE KEY entity_name (entity_name,parent_id),
+	KEY entity_path (entity_path(255)),
+	KEY FK_eav__entities_eav__entities (parent_id),
+	CONSTRAINT FK_eav__entities_eav__entities FOREIGN KEY (parent_id) REFERENCES eav__entities (id) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 
